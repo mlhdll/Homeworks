c58503e256a5df434e3529b021e5d6de2263936d
Melih Dal <2212101045@ogr.mehmetakif.edu.tr>
2022-12-06 19:11:20 +0300

    Update W7HomeworkX6.java

--- Week7/W7HomeworkX6.java
+++ Week7/W7HomeworkX6.java
@@ -15,6 +15,13 @@ public class W7HomeworkX6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        for (int i = 2; i <= 7; i++) {
+            for (int j = 2; j <= 7; j++) {
+                if ((i==2||i==3||i==5||i==7)&&(j==2||j==3||j==5||j==7)) {
+                    System.out.println(10*i+j);
+                }
+            }
+        }
     }
     
 }
